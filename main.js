b3265158a5994c28910ff4b033edf22afbe3483f
Brandon Montes <bmontes115@gmail.com>
2020-05-12 21:42:05 -0700

    Added audio file to be uesd later for the background, along with 2 background videos for both html files one for the main screen and the second for the character selection screen. Create new folder to organize the new files that being a grid and background css files along with a font one in its own folder. Then updated the html and css to fit the buttons and with the correct images and videos along with making sure they fit correctly

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 //API's to use for Smash Bros http://smashlounge.com/api/doc (all techniques:/api/techs/all, all characters:/api/chars/all, specific character techniques:/api/attack/char/1)
 
-//API's to use for Amiboo's https://www.amiiboapi.com/docs/ (characters: api/?character="character")
+//API's to use for Amiboo's https://www.amiiboapi.com/docs/ (characters: api/amiibo/?character="character")
