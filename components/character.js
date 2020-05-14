04e843801191ff8eaf343d2796f1d5baed0ff178
Brandon Montes <bmontes115@gmail.com>
2020-05-14 01:16:05 -0700

    Combined index and character html files to form one, added the funcitons to make one page appear while the other is hidden, almost got the api's functioning properly

--- components/character.js
+++ components/character.js
@@ -3,11 +3,34 @@
 //API's to use for Amiboo's https://www.amiiboapi.com/docs/ (characters: api/amiibo/?character="character")
 
 
+  function fighterSelected(id, charname, attacks) {
+    fetch('https://cors-anywhere.herokuapp.com/http://smashlounge.com/api/attack/char/' + id)
+    .then(res=>res.json())
+    .then(data=> console.log(data))
+    $.ajax({
+      url: 'https://cors-anywhere.herokuapp.com/http://smashlounge.com/api/attack/char/' + id,
+      data: {
+        "name": charname,
+        "attacks": attacks
+      },
+      success: chosenFighterSuccess,
+      fail: chosenFighterError
+    })
 
-function fighterSelected(id) {
+  }
 
-}
+  function chosenFighterSuccess() {
+    console.log(name, attacks)
+  }
 
-function getFighters(character) {
-  var fighter = document.querySelector('character')
-}
+   function chosenFighterError(err) {
+    console.log(err)
+  }
+
+
+// function amiiboSelected(id) {
+//   $.ajax({
+//     method: "GET",
+//     url: "https://www.amiiboapi.com/api/amiibo/?character=" + id
+//   })
+// }
--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 //API's to use for Smash Bros http://smashlounge.com/api/doc (all techniques:/api/techs/all, all characters:/api/chars/all, specific character techniques:/api/attack/char/1)
 
 //API's to use for Amiboo's https://www.amiiboapi.com/docs/ (characters: api/amiibo/?character="character")
+
+
+var page1 = document.getElementById('page-1')
+
+var page2 = document.getElementById('page-2')
+
+var exitPage = document.getElementById('exit')
+
+var meleePage = document.getElementById('melee')
+
+meleePage.addEventListener('click', meleeMenu)
+
+exitPage.addEventListener('click', mainMenu)
+
+function meleeMenu() {
+  page1.classList.add('hidden')
+  page2.classList.remove('hidden')
+
+}
+
+function mainMenu() {
+  page2.classList.add('hidden')
+  page1.classList.remove('hidden')
+}
